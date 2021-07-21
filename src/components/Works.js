245302fec115cf34ec616456c41aae30a971f9a4
Shiwanshu Shubham <shiwanshu.en@gmail.com>
2021-07-21 23:53:54 +0530

    added new project on Works.js

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,8 +1,18 @@
 import "./Works.css";
+import Img2 from "../images/project2.png";
 import Img3 from "../images/project3.png";
 import Card from "./Card";
 
 const projectsArr = [
+  {
+    title: "Marked Previewer",
+    img: Img2,
+    desc:
+      "Project created with marked.js, water.css and create-react-app.",
+    link: "https://viewmarkdown.netlify.app/",
+
+    github: 'https://github.com/shiwanshudev/marked_previewer'
+  },
   {
     title: "ReactJS Dynamic Quiz",
     img: Img3,
