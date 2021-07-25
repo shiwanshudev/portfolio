5a9aebad8c276b04e9ff1d6f7b70187dcb2d1b55
Shiwanshu Shubham <shiwanshu.en@gmail.com>
2021-07-25 21:30:21 +0530

    added project

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,14 +1,24 @@
 import "./Works.css";
+import Img1 from "../images/project1.png";
 import Img2 from "../images/project2.png";
 import Img3 from "../images/project3.png";
 import Card from "./Card";
 
 const projectsArr = [
   {
+    title: "Sales Invoice MERN",
+    img: Img1,
+    desc:
+      "Application created as a minor project for University which manages items in the Grocery Store and generates invoices which are stored in database. Project was designed and implemented from scratch with MERN stack.",
+    link: "https://sales-invoice-mern.netlify.app/",
+
+    github: ""
+  },
+  {
     title: "Marked Previewer",
     img: Img2,
     desc:
-      "Project created with marked.js, water.css and create-react-app.",
+      "Project created with marked.js, water.css and create-react-app. A simple marked previewer created with ReactJS.",
     link: "https://viewmarkdown.netlify.app/",
 
     github: 'https://github.com/shiwanshudev/marked_previewer'
