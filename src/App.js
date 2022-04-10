972190eea8c78065d830f50c8fea7998592e7c01
shiwanshudev <shiwanshudev@protonmail.com>
2022-04-10 11:33:03 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
 import Projects from "./components/Projects";
-import { Wip } from "./components/Wip";
 import Skills from "./components/Skills";
 import About from "./components/About";
 
@@ -15,6 +14,17 @@ const App = () => {
       <Skills />
       <Projects />
       <About />
+      <footer
+        style={{
+          backgroundColor: "slateblue",
+          color: "var(--lightSlateBlue)",
+          textAlign: "center",
+          padding: "1rem",
+          fontSize: "0.8rem",
+        }}
+      >
+        Designed and Developed by Shiwanshu &copy; 2022
+      </footer>
     </div>
   );
 };
