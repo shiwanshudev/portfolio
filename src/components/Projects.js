332fd6e7d353c80ab09c8f2e2e7cb6a976d30bdb
shiwanshudev <shiwanshudev@protonmail.com>
2022-04-18 20:06:15 +0530

    added data for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,75 +1,45 @@
 import "./Projects.css";
-import images from "./assets/images";
 import { SiGithub } from "react-icons/si";
+import projects from "./assets/data";
+
+const renderedProjects = projects.map((project) => {
+  return (
+    <div className="card">
+      <img src={project.img} alt={project.title} />
+      <h3>{project.title}</h3>
+      <p>{project.desc}</p>
+      <div className="links">
+        <a
+          className="view-link"
+          href={project.link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          View
+        </a>
+        <a
+          className="github-link"
+          rel="noreferrer"
+          target="_blank"
+          href={project.github}
+        >
+          <SiGithub />
+        </a>
+      </div>
+      <div className="technologies">
+        {project.tags.map((tag) => (
+          <span>{tag}</span>
+        ))}
+      </div>
+    </div>
+  );
+});
 
 const Projects = () => {
   return (
     <div id="projects" className="projects container">
       <h1>Projects</h1>
-      <div className="cards">
-        <div className="card">
-          <img src={images.projectMarkdown} alt="Markdown Project" />
-          <h3>Markdown Previewer</h3>
-          <p>
-            Project created with marked.js, water.css and create-react-app. A
-            simple marked previewer created with ReactJS.
-          </p>
-          <div className="links">
-            <a
-              className="view-link"
-              href="https://viewmarkdown.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View
-            </a>
-            <a
-              className="github-link"
-              rel="noreferrer"
-              target="_blank"
-              href="https://github.com/shiwanshudev/marked_previewer"
-            >
-              <SiGithub />
-            </a>
-          </div>
-          <div className="technologies">
-            <span>MarkedJS</span>
-            <span>Water CSS</span>
-            <span>ReactJS</span>
-          </div>
-        </div>
-        <div className="card">
-          <img src={images.projectQuiz} alt="Quiz Project" />
-          <h3>Quiz Trivia API</h3>
-          <p>
-            Side project created for practicing my skills. The application uses
-            ReactJS, Open Trivia DB API, Axios and Bootstrap.
-          </p>
-          <div className="links">
-            <a
-              className="view-link"
-              href="https://dynamicquizreact.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View
-            </a>
-            <a
-              className="github-link"
-              rel="noreferrer"
-              target="_blank"
-              href="https://github.com/shiwanshudev/quiz-app"
-            >
-              <SiGithub />
-            </a>
-          </div>
-          <div className="technologies">
-            <span>ReactJS</span>
-            <span>Bootstrap</span>
-            <span>Axios</span>
-          </div>
-        </div>
-      </div>
+      <div className="cards">{renderedProjects}</div>
     </div>
   );
 };
---
+++ src/components/assets/data.js
@@ -0,0 +1,20 @@
+import images from "./images";
+
+export default [
+  {
+    title: "Markdown Previewer",
+    desc: "Project created with marked.js, water.css and create-react-app. A simple marked previewer created with ReactJS.",
+    link: "https://viewmarkdown.netlify.app/",
+    github: "https://github.com/shiwanshudev/marked_previewer",
+    tags: ["MarkedJS", "Water CSS", "ReactJS"],
+    img: images.projectMarkdown,
+  },
+  {
+    title: "Quiz Trivia API",
+    desc: " Side project created for practicing my skills. The application uses ReactJS, Open Trivia DB API, Axios and Bootstrap.",
+    link: "https://dynamicquizreact.netlify.app/",
+    github: "https://github.com/shiwanshudev/quiz-app",
+    tags: ["ReactJS", "Bootstrap", "Axios"],
+    img: images.projectQuiz,
+  },
+];
